b4ec095bdd81d9a9716b3cfb415e0cb8ffb8fa2f
aspiearchitect <aspiearchitect@gmail.com>
2015-05-18 21:04:49 +0100

    New DaitchMokotoff class (still to be completed)

---
+++ PhoneticAlgorithms/DaitchMokotoff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DaitchMokotoff : GeneralPhonetics
+{
+    public DaitchMokotoff(string inputString) : base(inputString) { }
+
+
+    public override void Iterate()
+    {
+        base.Iterate();
+    }
+
+    public override void PhoneticAlgorithm()
+    {
+        if (IsStartOfWord())
+        {
+            if (!(ProcessWordStart()))
+            {
+                _outputArray[_validCharacterPosition++] = _inputArray[_currentCharacterPosition];
+            }
+
+        }
+
+    }
+
+    /// <summary>
+    /// Carries out any character substitutions necessary at the start of a word.  Where a valid substitution is not detected
+    /// the responsibility for handling the character is handled by the calling method.
+    /// </summary>
+    /// <returns>TRUE = The word start substitution algorithm successfully identified and replaced an 'n' character word start.</returns>
+    private bool ProcessWordStart()
+    {
+        bool returnValue = false;
+        for (int i = 0; i < DaitchMokotoffUtility.WORDSTART.Length; i++)
+        {
+            if (ArrayMatchFromPosition(_currentCharacterPosition, DaitchMokotoffUtility.WORDSTART[i][0]) == DaitchMokotoffUtility.WORDSTART[i][0].Length)
+            {
+                returnValue = ArrayReplaceFromPosition(DaitchMokotoffUtility.WORDSTART[i][0], DaitchMokotoffUtility.WORDSTART[i][1]);
+                if (returnValue) { break; }
+            }
+        }
+        return returnValue;
+
+    }
+
+}
---
+++ PhoneticsTests/DaitchMokotoffTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PhoneticsTests
+{
+    [TestClass]
+    public class DaitchMokotoffSoundex
+    {
+        [TestMethod]
+        public void DM_ZeroStart()
+        {
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "AI"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "AJ"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "AU"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "AY"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "EI"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "EJ"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "EY"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "OI"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "OJ"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "OY"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "UE"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "UI"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "UJ"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "UY"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "A"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "E"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "I"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "O"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "U"));
+            Assert.AreEqual("0", Phonetics.LongPhonetic(3, "Y"));
+        }
+        [TestMethod]
+        public void DM_OneStart()
+        {
+            Assert.AreEqual("1", Phonetics.LongPhonetic(3, "IA"));
+            Assert.AreEqual("1", Phonetics.LongPhonetic(3, "IE"));
+            Assert.AreEqual("1", Phonetics.LongPhonetic(3, "IO"));
+            Assert.AreEqual("1", Phonetics.LongPhonetic(3, "IU"));
+            Assert.AreEqual("1", Phonetics.LongPhonetic(3, "EU"));
+            Assert.AreEqual("1", Phonetics.LongPhonetic(3, "J"));
+        }
+        [TestMethod]
+        public void DM_TwoStart()
+        {
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SCHTSCH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SCHTSH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SCHTSH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SCTCH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "STSCH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SHTSH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SHCH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "STCH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "STRZ"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "STRS"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "STSH"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SZCZ"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SZCS"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SCHD"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SCHT"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SHT"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SHD"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SZD"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SZT"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "ST"));
+            Assert.AreEqual("2", Phonetics.LongPhonetic(3, "SC"));
+        }
+        [TestMethod]
+        public void DM_ThreeStart()
+        {
+            Assert.AreEqual("3", Phonetics.LongPhonetic(3, "DT"));
+            Assert.AreEqual("3", Phonetics.LongPhonetic(3, "TH"));
+            Assert.AreEqual("3", Phonetics.LongPhonetic(3, "T"));
+            Assert.AreEqual("3", Phonetics.LongPhonetic(3, "D"));
+        }
+        [TestMethod]
+        public void DM_SixStart()
+        {
+            Assert.AreEqual("6", Phonetics.LongPhonetic(3, "M"));
+            Assert.AreEqual("6", Phonetics.LongPhonetic(3, "N"));
+        }
+        [TestMethod]
+        public void DM_SevenStart()
+        {
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "FB"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "PH"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "PF"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "B"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "F"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "P"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "V"));
+            Assert.AreEqual("7", Phonetics.LongPhonetic(3, "W"));
+        }
+        [TestMethod]
+        public void DM_EightStart()
+        {
+            Assert.AreEqual("8", Phonetics.LongPhonetic(3, "L"));
+        }
+        [TestMethod]
+        public void DM_NineStart()
+        {
+            Assert.AreEqual("9", Phonetics.LongPhonetic(3, "R"));
+        }
+    }
+}
