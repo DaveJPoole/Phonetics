4963cebb8351942fe76bef9ad6d62fb23e7d5b88
aspiearchitect <aspiearchitect@gmail.com>
2015-05-24 14:22:33 +0100

    Initial creation

---
+++ PhoneticAlgorithms/DaitchMokotoffUtility_VOWEL.cs
@@ -0,0 +1,451 @@
+﻿/// <summary>
+/// Contains the source to target arrays used to produce the Daith Mokotoff phonetic algorithm
+/// </summary>
+/// <remarks>Note that testing these substitutions should be done by filling in the longest array first.  To give an example let us suppose that you are testing WORDSTART.
+/// Including a Z=4 mapping will mean that all word start substitutions beginning with Z will pass even though the multi-character array substitutions have yet to be written.</remarks>
+public static partial class DaitchMokotoffUtility
+{
+    public static char[][][] AFTERVOWEL = new char[][][]
+    {
+        new char[][]{
+            new char[] { 'S', 'C','H','T','S','C','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S', 'C','H','T','S','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S', 'C','H','T','C','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','H','T','C','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','H','T','S','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','T','S','C','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'T','T','S','C','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'S','H','C','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','T','S','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','T','C','H' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','T','R','Z' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','T','R','Z' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','Z','C','Z' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','Z','C','S' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','C','H','T' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','C','H','D' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'T','R','C','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','T','S','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','T','C','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'Z','S','C','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'Z','H','S','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'S','H','D' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','H','T' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','Z','D' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'S','Z','T' },
+            new char[] {'2'}
+        },
+        new char[][]{
+            new char[] { 'C','S','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'C','Z','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','R','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','R','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','S','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','Z','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','Z','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'S','C','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','C','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','Z','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','S','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','S','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','R','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','T','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','T','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','R','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'C','H','S' },
+            new char[] {'5'}
+        },
+        new char[][]{
+            new char[] { 'A', 'I' },
+            new char[] {'0'}
+        },
+        new char[][]{
+            new char[]{'A','J'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'A','Y'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'A','U'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[] { 'E', 'I' },
+            new char[] {'0'}
+        },
+        new char[][]{
+            new char[]{'E','J'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'E','Y'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[] { 'I', 'A' },
+            new char[] {'1'}
+        },
+        new char[][]{
+            new char[] { 'I', 'E' },
+            new char[] {'1'}
+        },
+        new char[][]{
+            new char[] { 'I', 'O' },
+            new char[] {'1'}
+        },
+        new char[][]{
+            new char[] { 'I', 'U' },
+            new char[] {'1'}
+        },
+        new char[][]{
+            new char[] { 'E', 'U' },
+            new char[] {'1'}
+        },
+        new char[][]{
+            new char[] { 'O', 'I' },
+            new char[] {'0'}
+        },
+        new char[][]{
+            new char[]{'O','J'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'O','Y'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[] { 'U', 'E' },
+            new char[] {'0'}
+        },
+        new char[][]{
+            new char[] { 'U', 'I' },
+            new char[] {'0'}
+        },
+        new char[][]{
+            new char[]{'U','J'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'U','Y'},
+            new char[]{'0'}
+        },
+         new char[][]{
+            new char[] { 'S','T' },
+            new char[] {'2'}
+        },
+         new char[][]{
+            new char[] { 'S','C' },
+            new char[] {'2'}
+        },
+         new char[][]{
+            new char[] { 'D','T' },
+            new char[] {'3'}
+        },
+         new char[][]{
+            new char[] { 'T','H' },
+            new char[] {'3'}
+        },
+         new char[][]{
+            new char[] { 'C','S' },
+            new char[] {'4'}
+        },
+         new char[][]{
+            new char[] { 'C','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'D','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'S','H' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','Z' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','S' },
+            new char[] {'4'}
+        },
+        new char[][]{
+            new char[] { 'T','C' },
+            new char[] {'4'}
+        },
+         new char[][]{
+            new char[] { 'M','N' },
+            new char[] {}
+        },
+         new char[][]{
+            new char[] { 'N','M' },
+            new char[] {}
+        },
+         new char[][]{
+            new char[] { 'F','B' },
+            new char[] {'7'}
+        },
+         new char[][]{
+            new char[] { 'P','H' },
+            new char[] {'7'}
+        },
+         new char[][]{
+            new char[] { 'P','F' },
+            new char[] {'7'}
+        },
+         new char[][]{
+            new char[] { 'Z','H' },
+            new char[] {'4'}
+        },
+         new char[][]{
+            new char[] { 'Z','S' },
+            new char[] {'4'}
+        },
+         new char[][]{
+            new char[] { 'C','H' },
+            new char[] {'5'}
+        },
+         new char[][]{
+            new char[] { 'C','K' },
+            new char[] {'5'}
+        },
+         new char[][]{
+            new char[] { 'K','S' },
+            new char[] {'5'}
+        },
+         new char[][]{
+            new char[] { 'K','H' },
+            new char[] {'5'}
+        },
+       new char[][]{
+            new char[]{'A'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'E'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'I'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'O'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'U'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'Y'},
+            new char[]{'0'}
+        },
+        new char[][]{
+            new char[]{'J'},
+            new char[]{'1'}
+        },
+        new char[][]{
+            new char[]{'D'},
+            new char[]{'3'}
+        },
+        new char[][]{
+            new char[]{'T'},
+            new char[]{'3'}
+        },
+        new char[][]{
+            new char[]{'S'},
+            new char[]{'4'}
+        },
+        new char[][]{
+            new char[]{'Z'},
+            new char[]{'4'}
+        },
+        new char[][]{
+            new char[]{'C'},
+            new char[]{'5'}
+        },
+        new char[][]{
+            new char[]{'G'},
+            new char[]{'5'}
+        },
+        new char[][]{
+            new char[]{'K'},
+            new char[]{'5'}
+        },
+        new char[][]{
+            new char[]{'Q'},
+            new char[]{'5'}
+        },
+        new char[][]{
+            new char[]{'H'},
+            new char[]{'5'}
+        },
+        new char[][]{
+            new char[]{'X'},
+            new char[]{'5'}
+        },
+        new char[][]{
+            new char[]{'M'},
+            new char[]{'6'}
+        },
+        new char[][]{
+            new char[]{'N'},
+            new char[]{'6'}
+        },
+        new char[][]{
+            new char[]{'B'},
+            new char[]{'7'}
+        },
+        new char[][]{
+            new char[]{'F'},
+            new char[]{'7'}
+        },
+        new char[][]{
+            new char[]{'P'},
+            new char[]{'7'}
+        },
+        new char[][]{
+            new char[]{'V'},
+            new char[]{'7'}
+        },
+        new char[][]{
+            new char[]{'W'},
+            new char[]{'7'}
+        },
+        new char[][]{
+            new char[]{'L'},
+            new char[]{'8'}
+        },
+        new char[][]{
+            new char[]{'R'},
+            new char[]{'9'}
+        }
+    };
+}
