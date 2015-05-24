01bebfdb4a0b9474f28384186c708fc97ed549d3
aspiearchitect <aspiearchitect@gmail.com>
2015-05-24 20:30:24 +0100

    Complete set of after vowel subsitutions

--- PhoneticAlgorithms/DaitchMokotoffUtility_VOWEL.cs
+++ PhoneticAlgorithms/DaitchMokotoffUtility_VOWEL.cs
@@ -1,35 +1,33 @@
 ﻿/// <summary>
 /// Contains the source to target arrays used to produce the Daith Mokotoff phonetic algorithm
 /// </summary>
-/// <remarks>Note that testing these substitutions should be done by filling in the longest array first.  To give an example let us suppose that you are testing WORDSTART.
-/// Including a Z=4 mapping will mean that all word start substitutions beginning with Z will pass even though the multi-character array substitutions have yet to be written.</remarks>
 public static partial class DaitchMokotoffUtility
 {
     public static char[][][] AFTERVOWEL = new char[][][]
     {
         new char[][]{
             new char[] { 'S', 'C','H','T','S','C','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S', 'C','H','T','S','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S', 'C','H','T','C','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','H','T','C','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','H','T','S','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','T','S','C','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'T','T','S','C','H' },
@@ -37,39 +35,39 @@ public static partial class DaitchMokotoffUtility
         },
         new char[][]{
             new char[] { 'S','H','C','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','T','S','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','T','C','H' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','T','R','Z' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
-            new char[] { 'S','T','R','Z' },
-            new char[] {'2'}
+            new char[] { 'S','T','R','S' },
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','Z','C','Z' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','Z','C','S' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
         new char[][]{
             new char[] { 'S','C','H','T' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
         new char[][]{
             new char[] { 'S','C','H','D' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
         new char[][]{
             new char[] { 'T','R','C','H' },
@@ -93,19 +91,19 @@ public static partial class DaitchMokotoffUtility
         },
         new char[][]{
             new char[] { 'S','H','D' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
         new char[][]{
             new char[] { 'S','H','T' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
         new char[][]{
             new char[] { 'S','Z','D' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
         new char[][]{
             new char[] { 'S','Z','T' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
         new char[][]{
             new char[] { 'C','S','Z' },
@@ -173,91 +171,91 @@ public static partial class DaitchMokotoffUtility
         },
         new char[][]{
             new char[] { 'C','H','S' },
-            new char[] {'5'}
+            new char[] {'5','4'}
         },
         new char[][]{
             new char[] { 'A', 'I' },
-            new char[] {'0'}
+            new char[] {'1'}
         },
         new char[][]{
             new char[]{'A','J'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
         new char[][]{
             new char[]{'A','Y'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
         new char[][]{
             new char[]{'A','U'},
-            new char[]{'0'}
+            new char[]{'7'}
         },
         new char[][]{
             new char[] { 'E', 'I' },
-            new char[] {'0'}
+            new char[] {'1'}
         },
         new char[][]{
             new char[]{'E','J'},
-            new char[]{'0'}
+            new char[]{'1'}
+        },
+        new char[][]{
+            new char[]{'E','U'},
+            new char[]{'1'}
         },
         new char[][]{
             new char[]{'E','Y'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
         new char[][]{
             new char[] { 'I', 'A' },
-            new char[] {'1'}
+            new char[] {}
         },
         new char[][]{
             new char[] { 'I', 'E' },
-            new char[] {'1'}
+            new char[] {}
         },
         new char[][]{
             new char[] { 'I', 'O' },
-            new char[] {'1'}
+            new char[] {}
         },
         new char[][]{
             new char[] { 'I', 'U' },
-            new char[] {'1'}
-        },
-        new char[][]{
-            new char[] { 'E', 'U' },
-            new char[] {'1'}
+            new char[] {}
         },
         new char[][]{
             new char[] { 'O', 'I' },
-            new char[] {'0'}
+            new char[] {'1'}
         },
         new char[][]{
             new char[]{'O','J'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
         new char[][]{
             new char[]{'O','Y'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
         new char[][]{
             new char[] { 'U', 'E' },
-            new char[] {'0'}
+            new char[] {}
         },
         new char[][]{
             new char[] { 'U', 'I' },
-            new char[] {'0'}
+            new char[] {'1'}
         },
         new char[][]{
             new char[]{'U','J'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
         new char[][]{
             new char[]{'U','Y'},
-            new char[]{'0'}
+            new char[]{'1'}
         },
          new char[][]{
             new char[] { 'S','T' },
-            new char[] {'2'}
+            new char[] {'4','3'}
         },
          new char[][]{
             new char[] { 'S','C' },
-            new char[] {'2'}
+            new char[] {'4'}
         },
          new char[][]{
             new char[] { 'D','T' },
@@ -301,11 +299,11 @@ public static partial class DaitchMokotoffUtility
         },
          new char[][]{
             new char[] { 'M','N' },
-            new char[] {}
+            new char[] {'6','6'}
         },
          new char[][]{
             new char[] { 'N','M' },
-            new char[] {}
+            new char[] {'6','6'}
         },
          new char[][]{
             new char[] { 'F','B' },
@@ -337,7 +335,7 @@ public static partial class DaitchMokotoffUtility
         },
          new char[][]{
             new char[] { 'K','S' },
-            new char[] {'5'}
+            new char[] {'5','4'}
         },
          new char[][]{
             new char[] { 'K','H' },
@@ -345,27 +343,27 @@ public static partial class DaitchMokotoffUtility
         },
        new char[][]{
             new char[]{'A'},
-            new char[]{'0'}
+            new char[]{}
         },
         new char[][]{
             new char[]{'E'},
-            new char[]{'0'}
+            new char[]{}
         },
         new char[][]{
             new char[]{'I'},
-            new char[]{'0'}
+            new char[]{}
         },
         new char[][]{
             new char[]{'O'},
-            new char[]{'0'}
+            new char[]{}
         },
         new char[][]{
             new char[]{'U'},
-            new char[]{'0'}
+            new char[]{}
         },
         new char[][]{
             new char[]{'Y'},
-            new char[]{'0'}
+            new char[]{}
         },
         new char[][]{
             new char[]{'J'},
@@ -409,7 +407,7 @@ public static partial class DaitchMokotoffUtility
         },
         new char[][]{
             new char[]{'X'},
-            new char[]{'5'}
+            new char[]{'5','4'}
         },
         new char[][]{
             new char[]{'M'},
