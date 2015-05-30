ec3a280ebeeabf8583cf868d94d03c6612fb7de7
aspiearchitect <aspiearchitect@gmail.com>
2015-05-30 22:26:59 +0100

    Added tests for the underlying base classes rather than the specific phonetic algorithms

---
+++ PhoneticsTests/BaseTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PhoneticsTests
+{
+    [TestClass]
+    public class BaseTests
+    {
+        [TestMethod]
+        public void Base_Start()
+        {
+            BasePhonetics bp = new BasePhonetics("Dave Poole", false);
+            bp.Iterate();
+            Assert.AreEqual("DAVE POOLE", bp.ReadOutput());
+            Assert.AreNotEqual("Dave Poole", bp.ReadOutput());
+        }
+    }
+}
